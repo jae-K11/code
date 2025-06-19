318840b7eb4a0f8b9c0ad64d9d189d9846510500
jae-K11 <jojo2852@naver.com>
2025-06-19 17:53:50 +0900

    250619

--- forest/js/common.js
+++ forest/js/common.js
@@ -1,1 +1,20 @@
 /* header, footer 공통요소에 들어가는 javascript/jquery */
+
+$(document).ready(function(){
+    let scrolling
+
+    scroll_chk()
+
+    $(window).scroll(function(){
+        scroll_chk()
+    })
+    function scroll_chk(){
+        scrolling = $(window).scrollTop()
+        console.log(scrolling)
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+        }else{
+            $('header').removeClass('fixed')
+        }
+    }
+})
--- forest/js/main.js
+++ forest/js/main.js
@@ -86,5 +86,29 @@ $(document).ready(function(){
 		
 	
 	});
+
+
+/******************** story의 swiper :시작 **********************/
+
+const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	slidesPerView: "auto", /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+	breakpoints: {
+		769: {    /* 1281~768 이상일때 적용 */
+			slidesPerView: 3,    /* 표 갯수 */
+			spaceBetween: 24, /* 표 사이 여백*/
+		},
+		1281: {    /* 1280px 이상일때 적용 */
+			slidesPerView: 4,    /* 표 갯수 */
+			spaceBetween: 24, /* 표 사이 여백*/
+		},
+	},
+	//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+});
+
+
+/******************** story의 swiper :끝 **********************/
+	
 	
 })/*$(document)*/
