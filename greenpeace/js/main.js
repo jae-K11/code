0999cbee04af910ef84d5c517b49a9edf71b0717
jae-K11 <jojo2852@naver.com>
2025-06-27 02:05:34 +0900

    250627  포폴

--- greenpeace/js/main.js
+++ greenpeace/js/main.js
@@ -4,14 +4,23 @@ $(document).ready(function(){
 
         const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-            //autoplay: {  /* 팝업 자동 실행 */
-             //   delay: 2500,
-             //   disableOnInteraction: true,
-            //},
+            autoplay: {  /* 팝업 자동 실행 */
+               delay: 2500,
+               disableOnInteraction: true,
+            },
 
-            //effect: "fade", /* fade 효과 */
+            effect: "fade", /* fade 효과 */
 
             loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+            
+            	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+                    el: '.visual .ctrl_wrap .pagination', /* 해당 요소의 class명 */
+                    clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+                    type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+                    renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+                        return '<span class="' + className + '">' + (index + 1) + "</span>";
+                    },
+                },
             
 
             navigation: {  /* 이전, 다음 버튼 */
@@ -20,8 +29,8 @@ $(document).ready(function(){
             },
 
         });
-        // swiper.autoplay.stop();  /* 일시정지 기능 */
-        // swiper.autoplay.start();  /* 재생 기능 */
+        swiper.autoplay.stop();  /* 일시정지 기능 */
+        swiper.autoplay.start();  /* 재생 기능 */
 
         /*************visual 의 swiper 연결 : 끝 ************/
 })//$(document).ready
