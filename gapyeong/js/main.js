40c51f140aa3d5e31cd5d20f40507f651ad54c13
jae-K11 <jojo2852@naver.com>
2025-07-11 00:54:06 +0900

    25.07.11 가평

--- gapyeong/js/main.js
+++ gapyeong/js/main.js
@@ -68,13 +68,12 @@ $(document).ready(function(){
 
     const tour_swiper = new Swiper('.tour .swiper', { /* 팝업을 감싼는 요소의 class명 */
         slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        spaceBetween: 24, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
-            360: {    /* 1560px 이상일때 적용 */
-                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            320: {    /* 1560px 이상일때 적용 */
+                slidesPerView: 'auto',
                 spaceBetween: 24,
             },
-
         },
         centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
