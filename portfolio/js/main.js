6ec831b211570c61d0e7eb030a6760f3ab89dfd4
jae-K11 <jojo2852@naver.com>
2025-07-23 16:28:15 +0900

    250723 포폴

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -32,10 +32,26 @@ $(document).ready(function(){
             if (destination.index === 0) {
             header.style.display = 'none';
             }
-
         },
-        
-    
         responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+
+    });//fullpage
+
+    
+    $(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+      $('.cursor').css('left', e.pageX + 'px');
+      $('.cursor').css('top', e.pageY + 'px');
     });
-})//fullpage
+    $('.project01_wrap a.pj01').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+      $('.cursor').toggleClass('on');
+    });
+
+    $(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+      $('.cursor').css('left', e.pageX + 'px');
+      $('.cursor').css('top', e.pageY + 'px');
+    });
+    $('.project02_wrap a.pj02').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+      $('.cursor').toggleClass('on');
+    });
+
+})
