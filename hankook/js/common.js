aea8c6532d675fc5d83c99a8d0210a184aa733cf
jae-K11 <jojo2852@naver.com>
2025-08-08 02:35:24 +0900

    한국병원 포폴

---
+++ hankook/js/common.js
@@ -0,0 +1,7 @@
+let device_status // pc인지 모바일인지 구분하는 값 
+let scrolling // 브라우저가 스크롤 된 값
+let scroll_prev //이전에 스크롤 된값
+let window_w // 브라우저의 넓이 값
+let mobile_size = 1024 //모바일로 변경되는 사이트
+let menu_open //모바일에서 사용할 메뉴가 열렸는지 여부를 저장 
+
