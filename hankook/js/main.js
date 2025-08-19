13f257e0589cf66bdaa79e5f6b9a7e6ce32c96bd
jae-K11 <jojo2852@naver.com>
2025-08-20 04:05:24 +0900

    포폴

--- hankook/js/main.js
+++ hankook/js/main.js
@@ -69,15 +69,17 @@ $(document).ready(function(){
 
 			$('.notice .list .tab_content .tab_item').removeClass('active')
 			$('.notice .list .tab_content').find('#'+notice_content).addClass('active')
+			
+			$('.notice .list .tab_list ul li').removeClass('active')
+			$(this).addClass('active')
+
+			$('.notice .list .tab_list ul li button span').text('')
+			$(this).find('span').text('선택됨')
+
+			$('.notice .list .tab_list ul li').attr('aria-selected', 'false')
+			$(this).attr('aria-selected', 'true')
 		}
 	})
-
-
-
-
-
-
-
 
 /*************notice 의 tap: 끝 *************/
 
@@ -85,30 +87,24 @@ $(document).ready(function(){
 /********notice의 card swiper연결: 시작 ***********/
 const card_swiper = new Swiper('.card .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-	// autoplay: {  /* 팝업 자동 실행 */
-	// 	delay: 2500,
-	// 	disableOnInteraction: true,
-	// },
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 2200,
+		disableOnInteraction: true,
+	},
 
 	//effect: "fade", /* fade 효과 */
 
 	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
-	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-		    return '<span class="' + className + '">' + (index + 1) + "</span>";
-		},
-	},
+	// pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+	// 	el: '.swiper-pagination', /* 해당 요소의 class명 */
+	// 	clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+	// 	type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+	// 	renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+	// 	    return '<span class="' + className + '">' + (index + 1) + "</span>";
+	// 	},
+	// },
 	
-
-	navigation: {  /* 이전, 다음 버튼 */
-		nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
-		prevEl: '.swiper-button-prev',  
-	},
-
 });
 /********notice의 card swiper 연결: 끝 ***********/
 
